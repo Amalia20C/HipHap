42ea4e6b9443736927011cffbc8c286088cea68e
MadaPas <44397138+MadaPas@users.noreply.github.com>
2018-12-05 19:12:51 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,25 +1,27 @@
 public class Event {
 
-    private String id;
-    private String name;
-    private String type;
-    private String date;
-    private String location;
-    private Double eventPrice;
-    
-         public Event() {
+ private String id;
+ private String name;
+ private String type;
+ private String date;
+ private String location;
+ private String eventPrice;
+ private String employee;
 
-     }
-  
-    public Event (String id, String name, String type, String date, String location, double eventPrice ) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.date = date;
-        this.location = location;
-        this.eventPrice = eventPrice;
-    }
-    /* === setters ===  */
+ public Event() {
+
+ }
+
+ public Event(String id, String name, String type, String date, String location, String employee, String eventPrice) {
+  this.id = id;
+  this.name = name;
+  this.type = type;
+  this.date = date;
+  this.location = location;
+  this.employee = employee;
+  this.eventPrice = eventPrice;
+ }
+ /* === setters ===  /
   public void setId(String id) {
     this.id = id;
   }
@@ -35,30 +37,40 @@ public class Event {
     public void setLocation(String location) {
     this.location = location;
   }
-    public void setEventPrice(double eventPrice) {
+    public void setEventPrice(String eventPrice) {
     this.eventPrice = eventPrice;
   }
+   public void setEmployee(String employee) {
+      this.employee = employee;
+      }
+
+    / === getters === */
+ public String getEmployee() {
+  return this.employee;
+ }
+ public String getId() {
+  return id;
+ }
+
+ public String getName() {
+  return name;
+ }
+ public String getType() {
+  return type;
+ }
+ public String getDate() {
+  return date;
+ }
+ public String getLocation() {
+  return location;
+ }
+ public String getEventPrice() {
+  return eventPrice;
+ }
+
+ public void eventList() {
+  System.out.println("ID: " + this.id + " Name: " + this.name + " Type: " + this.type + " Date: " + this.date + " Location: " + this.location + " Organiser: " + this.employee + " Price: " + this.eventPrice);
+ }
 
 
-    /* === getters === */
-       public String getId() {
-        return id;
-    }
-
-        public String getName() {
-        return name;
-    }
-        public String getType() {
-        return type;
-    }
-        public String getDate() {
-        return date;
-    }
-        public String getLocation() {
-        return location;
-    }
-        public double getEventPrice() {
-        return eventPrice;
-    }
-   
 }
